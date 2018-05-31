b8cf9243fe67650ea4092deca83d5d8048275c94
jordanrios94 <jordanrios.software@gmail.com>
2018-05-31 19:57:14 +0100

    created last server api

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 ## Local Set up
 - ```npm install -g firebase-tools```
 - ```firebase login```
+
+## Deploy
+Please do not run deployment without consent of the team on slack. Taking down an api with bad JS is no cool.
+- ```firebase deploy```
---
+++ functions/services/firestore/create_chat_record.js
@@ -0,0 +1,42 @@
+const admin = require('firebase-admin');
+
+module.exports = (req, res) => {
+    if (!req.body.userId) {
+        return res.status(422).send({ error: 'User ID is missing' });
+    }
+
+    if (!req.body.friendUserID) {
+        return res.status(422).send({ error: 'Friend User ID is missing' });
+    }
+
+    if (!req.body.roomId) {
+        return res.status(422).send({ error: 'Room ID is missing' });
+    }
+
+    const { userId, friendUserID } = req.body;
+
+    const currentUserRef = db.collection('users').doc(userId);
+    const friendRef = db.collection('users').doc(friendUserID);
+    const batch = db.batch();
+
+    const currentUserContactRef = currentUserRef
+        .collection('contacts')
+        .doc(friendUserID);
+
+    const friendContactRef = friendRef
+        .collection('contacts')
+        .doc(userId);
+
+    batch.update(currentUserContactRef, { room_id: roomId });
+    batch.update(friendContactRef, { room_id: roomId });
+
+    return batch.commit()
+        .then(() => {
+            console.log('Successfully created room for direct messaging for users (' + userId + ') and (' + friendUserID + ')');
+            return res.send({ success: true });
+        })
+        .catch(error => {
+            console.log('Error performing batch updates on contacts (' + userId + ' and ' + friendUserID + ') to store room (' + roomId + ')', error);
+            return res.status(422).send({ error });
+        });
+}
